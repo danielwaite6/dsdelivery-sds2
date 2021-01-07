1af0e36da8f513b9ff4ef6fed2d0cc2cff32ff60
daniel machado waite <danielwaite6@hotmail.com>
2021-01-07 01:11:47 -0300

    Done: Domain model, Entities and relationships, Object-relational mapping and Seed.

--- backend/src/main/java/com/devsuperior/dsdelivery/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/entities/Order.java
@@ -5,22 +5,38 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address;
 	private Double latitude;
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	
 	// A Estrutura Set (conjunto/coleção ) não permite repetições do mesmo produto dentro do mesmo pedido.
+	@ManyToMany
+	@JoinTable(name = "tb_order_product", 
+	           joinColumns = @JoinColumn(name="order_id"), 
+	           inverseJoinColumns = @JoinColumn(name="product_id"))
 	private Set<Product> products = new HashSet<>(); 
 	
-	public Order() {
-		
-	}
+	public Order() {}
 
 	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
 		super();
