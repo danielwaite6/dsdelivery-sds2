b2bb2de04cba3b43e95e60d0b9913b10b2859df9
daniel machado waite <danielwaite6@hotmail.com>
2021-01-08 21:28:53 -0300

    adding several files to orders

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 
 function Orders() {
 
+    const [products, setProducts] = useState<Product[]>([]);
+    //console.log(products);
+    
+
+
+    useEffect(()=>{
+        ///console.log('Componente Orders iniciou.');
+        fetchProducts().then((response) => {
+            return setProducts(response.data);
+        })
+        .catch((err)=>{
+            return console.log(JSON.stringify(err));
+        });
+    }, []);
+
     return(
         <div className="orders-container">
             <StepsHeaders />
+            <ProductsList products={products} />
         </div>
     )
 
